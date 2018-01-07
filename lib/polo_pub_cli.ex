6b52452f76119e3cd6993c5e9edc61615200560d
erikstorm <erik.storm@metropolia.fi>
2018-01-07 15:45:25 +0200

    Remove useless comments

--- lib/polo_pub_cli.ex
+++ lib/polo_pub_cli.ex
@@ -1,19 +1,16 @@
 defmodule PoloPubCli do
   alias Utils.Printer
 
-  # Start supervisor
   def start(_type, _args) do
     Supervisor.start_link([], strategy: :one_for_one)
   end
 
-  # Program structure
   def main(args) do
     args
     |> parse_args
     |> handle_args
   end
 
-  # https://hexdocs.pm/elixir/OptionParser.html
   def parse_args(args) do
     case OptionParser.parse(args) do
       {[price: currency_pair], _, _} -> {:price, currency_pair}
@@ -24,7 +21,6 @@ defmodule PoloPubCli do
     end
   end
 
-  # Get price of currency pair
   def handle_args({:price, currency_pair}) do
     data = ProgressBar.render_spinner [
       text: "Fetching pricedata for #{currency_pair}…",
@@ -37,7 +33,6 @@ defmodule PoloPubCli do
     Printer.data_message({:price, data, currency_pair})
   end
 
-  # Live watch price of currency pair
   def handle_args({:watch, currency_pair}) do
     data = ProgressBar.render_spinner [
       text: "Updating pricedata for #{currency_pair}…",
@@ -51,11 +46,10 @@ defmodule PoloPubCli do
 
     # Loop.
     :timer.sleep(15000)
-    IO.ANSI.clear # Does not seem to work.
+    IO.ANSI.clear # Clearing the terminal does not seem to work.
     handle_args({:watch, currency_pair})
   end
 
-  # Get ticker
   def handle_args(:ticker) do
     data = ProgressBar.render_spinner [
       text: "Fetching ticker…",
@@ -68,7 +62,6 @@ defmodule PoloPubCli do
     Printer.data_message({:ticker, data})
   end
 
-  # Help/usage instructions
   def handle_args(:help) do
     Printer.help_message
   end
